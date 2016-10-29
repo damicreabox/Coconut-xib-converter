5a5abbc0407292153513e51b1ced3d5667f3465b
Dami <dami@creabox.net>
2016-10-29 20:05:12 +0200

    Add command line  arguments.

--- Sources/Coconut-xib-converter/main.swift
+++ Sources/Coconut-xib-converter/main.swift
@@ -6,6 +6,39 @@ guard let url = URL(string: "Files/MyNewView.xib") else {
     exit(1)
 }
 
+let arguments = CommandLine.arguments
+
+var file = "Files/MyNewView.xib"
+var output = "/Users/dami/XCode/Creabox/Coconut/Sources/Coconut/Sample/"
+
+var index = 0
+let size = arguments.count
+while index < size {
+    
+    switch arguments[index] {
+        
+    case "file" :
+        index += 1
+        if (index < size) {
+            file = arguments[index]
+            print(" - \(arguments[index - 1]) : \(file)")
+        }
+        break
+        
+    case "output":
+        index += 1
+        if (index < size) {
+            output = arguments[index]
+            print(" - \(arguments[index - 1]) : \(output)")
+        }
+        break
+        
+    default:
+        break
+    }
+    index += 1
+}
+
 do {
         
     // Set Xib definition
@@ -15,7 +48,7 @@ do {
     let xibDefinitionWriter = UIDefinitionWriter(xibDefinition: xibDefinition)
     
     // Generate files
-    try xibDefinitionWriter.write(at: URL(fileURLWithPath: "/Users/dami/XCode/Creabox/Coconut/Sources/Coconut/Sample/"))
+    try xibDefinitionWriter.write(at: URL(fileURLWithPath: output))
         
 } catch {
     print("\(error)")
